162d654957bbf03fe43f0427037a0451517d6a1b
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-12-12 12:09:09 +0530

    midiEvents

--- MiniMiniMusicApp.java
+++ MiniMiniMusicApp.java
@@ -13,6 +13,20 @@ public class MiniMiniMusicApp{
 			Sequence seq = new Sequence(Sequence.PPQ,4);
 			Track track = seq.createTrack();
 			
-		}
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(a,1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b,16);
+            track.add(noteOff);
+
+            player.setSequence(seq);
+            player.start();
+		} catch(Exception ex){
+            ex.printStackTrace();
+        }
 	}
 }
